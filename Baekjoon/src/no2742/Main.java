2819b1e35abe75343c6375a0b478f31bd5a01ab6
SeungHyun <ssh9112@naver.com>
2018-01-15 18:45:24 +0900

    Solve no.2742
    Signed-off-by: SeungHyun <ssh9112@naver.com>

---
+++ Baekjoon/src/no2742/Main.java
@@ -0,0 +1,15 @@
+package no2742;
+
+import java.util.Scanner;
+
+public class Main{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		
+		int count = sc.nextInt();
+		
+		for(;count>0;count--)
+			System.out.println(count);
+			
+	}
+}
